a847e55b7d861fb767d5c5ebf19ad37c5ca28210
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-01-18 19:47:00 -0800

    Items 21-30 addded

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -17,8 +17,17 @@
             Console.WriteLine("9.People call me Danny");
             Console.WriteLine("10.I have an amazing girlfriend");
             Console.WriteLine("11.I'm the middle child\n12.I'm 5'7\n13.I enjoy spending time with my girl\n14.I love eating Mexican food\n15.I enjoy working with my girl\n16.I like working out\n17.I work at IN N OUT\n18.I like going on walks with my girl\n19.I like napping\n20.I play on a ps5");
-            Console.WriteLine("21.");
-
+            Console.WriteLine("21.I go to chruch");
+            Console.WriteLine("22.My old car's transmission broke on me");
+            Console.WriteLine("23.I played sports");
+            Console.WriteLine("24.I grew up in a small town");
+            Console.WriteLine("25Family is important for me");
+            Console.WriteLine("26.I plan to major in eletrical engineering");
+            Console.WriteLine("27.I enjoy problem sloving");
+            Console.WriteLine("28.I like learning how things work");
+            Console.WriteLine("29.I'm taking calculusII this semester");
+            Console.WriteLine("30.I want a successful with my girl");
+            Console.WriteLine(""
         }
     }
 }
