27ef60d56b01e1e693d4164c31ff01a28215f80e
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-01-18 16:38:50 -0800

    Added items 1-10

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -6,6 +6,17 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Here are 100 Things About Me");
+            Console.WriteLine("1.My name is Daniel Juarez");
+            Console.WriteLine("2.I graduated from Lemoore High School");
+            Console.WriteLine("3.I have 2 pet dogs");
+            Console.WriteLine("4.I am 19 years old");
+            Console.WriteLine("5.I drive a Honda");
+            Console.WriteLine("6.I enjoy going on trips with my girl");
+            Console.WriteLine("7.I'm good at soccer");
+            Console.WriteLine("8.I have 4 siblings");
+            Console.WriteLine("9.People call me Danny");
+            Console.WriteLine("10.I have an amazing girlfriend");
+            Console.WriteLine("\n");
 
         }
     }
