bb0c3cf721f23d8b33fcc6872d0b1d089d3b963c
Daniel Juarez <danieljuarez5575@my.whccd.edu>
2026-01-18 16:04:03 -0800

    The assignment was made public for GitHub

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Here are 100 Things About Me");
+
         }
     }
 }
